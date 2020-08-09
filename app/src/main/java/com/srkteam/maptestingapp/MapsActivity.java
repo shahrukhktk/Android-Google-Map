34967551eb13b1bd9e8b7a3890ea0106bf4fc9bf
shahrukhktk <Shahrukhktk760@gmail.com>
2020-08-09 09:32:31 -0700

    Map Practice 1. Working on Map Zoom, Tilt, Animate 2. Ristricting Map to some specific Areas

---
+++ app/src/main/java/com/srkteam/maptestingapp/MapsActivity.java
@@ -0,0 +1,112 @@
+package com.srkteam.maptestingapp;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.security.Permission;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final int PERMISSION_REQUEST_CODE = 9001;
+    private boolean mLocationPermissionGranted;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        if(mLocationPermissionGranted)
+        {
+            Toast.makeText(this, "Ready To Map", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+                }
+            }
+
+        }
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if(requestCode == PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED )
+        {
+            mLocationPermissionGranted = true;
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Permission Not Granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        gotoLocation(-34.0, 151.0);
+
+        //UI CONTROL ON MAP
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
+    }
+
+    private void gotoLocation(Double lat, Double lng)
+    {
+        LatLng latLng = new LatLng(lat, lng);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10.0f);
+        showMarker(latLng);
+
+        mMap.moveCamera(cameraUpdate);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+    }
+
+    private void showMarker(LatLng latLng)
+    {
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng).title("Sydney");
+        mMap.addMarker(markerOptions);
+    }
+
+}
