347b883f7478d9707a82f66b0e28ff50e4365f82
shahrukhktk <Shahrukhktk760@gmail.com>
2020-08-09 10:58:55 -0700

    Map Practice 1. Working on Map Zoom, Tilt, Animate 2. Ristricting Map to some specific Areas 3. GeoCoding an address to its coordinates (Search location) 4.

--- app/src/main/java/com/srkteam/maptestingapp/MapsActivity.java
+++ app/src/main/java/com/srkteam/maptestingapp/MapsActivity.java
@@ -6,9 +6,15 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.IInterface;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -22,7 +28,10 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.IOException;
 import java.security.Permission;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -32,8 +41,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private FloatingActionButton floatingActionButton;
     private Double KUSTLAT = 33.523400;
     private Double KUSTLNG = 71.445774;
-
-
+    private EditText searchAddress;
+    private Button btnLocate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +63,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         }
+
+        searchAddress = findViewById(R.id.searchlocation);
+        btnLocate = findViewById(R.id.btnlocate);
+        btnLocate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                geoLocate(view);
+            }
+        });
+
 
         floatingActionButton = findViewById(R.id.fab);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -140,5 +159,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         markerOptions.position(latLng).title("Sydney");
         mMap.addMarker(markerOptions);
     }
+
+    private void geoLocate(View view)
+    {
+        hideSoftKeyboard(view);
+
+        String locationName = searchAddress.getText().toString();
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+             List<Address> addressList = geocoder.getFromLocationName(locationName, 1);
+             if(addressList.size() > 0)
+             {
+                 Address address = addressList.get(0);
+                 gotoLocation(address.getLatitude(), address.getLongitude());
+                 mMap.addMarker(new MarkerOptions().position(new LatLng(address.getLatitude(), address.getLongitude())));
+             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void hideSoftKeyboard(View view)
+    {
+        InputMethodManager iim = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        iim.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+    }
+
 
 }
